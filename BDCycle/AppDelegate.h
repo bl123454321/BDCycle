21e6b626b3bdec444f054b8f49d64656dfed626f
林有亮 <lin9948030@126.com>
2016-08-01 11:18:13 +0800

    Initial Commit

---
+++ BDCycle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BDCycle
+//
+//  Created by 林有亮 on 16/8/1.
+//  Copyright © 2016年 林有亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BDCycle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BDCycle
+//
+//  Created by 林有亮 on 16/8/1.
+//  Copyright © 2016年 林有亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
